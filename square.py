4c7b478032ebb6b49c83eeba4ef7d2087ed5443c
Yacine Laghdiri <yacine.laghdiri@gmail.com>
2023-11-18 13:39:09 +0100

    feat: manage different aspect ratio and reduce size

--- square.py
+++ square.py
@@ -6,13 +6,16 @@ ORG_IMG = "./org"
 DEST_IMG = "./dest"
 
 BORDER_RATIO = 0.05
+ASPECT_RATIO = 4/5
+REDUCE_FACTOR = 2
 
 def load_images(path: str):
     images: List[Image.Image] = []
 
     for filename in os.listdir(path):
-        img = Image.open(f"{path}/{filename}")
-        images.append(img)
+        if(filename != ".DS_Store"):
+            img = Image.open(f"{path}/{filename}")
+            images.append(img)
     return images
 
 def save_images(images: List[Image.Image], path: str):
@@ -22,12 +25,14 @@ def save_images(images: List[Image.Image], path: str):
 def squarify(img: Image.Image):
     old_size = img.size
 
-    new_size = (round(max(old_size)*(1 + BORDER_RATIO)),round(max(old_size)*(1 + BORDER_RATIO)))
+    aspect_multiplicator = ASPECT_RATIO if old_size[0] < old_size[1] else 1/ASPECT_RATIO
+
+    new_size = (round(max(old_size)*(1 + BORDER_RATIO)*min(aspect_multiplicator, 1)),round(max(old_size)*(1 + BORDER_RATIO)*max(aspect_multiplicator, 1)))
     new_img = Image.new("RGB", new_size, "White")
     box = tuple((n - o) // 2 for n, o in zip(new_size, old_size))
     new_img.paste(img, box)
 
-    return new_img
+    return new_img.reduce(REDUCE_FACTOR)
 
 if __name__=="__main__":
     images = load_images(ORG_IMG)
