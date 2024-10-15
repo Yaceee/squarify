74adc6e3552337f7fbc7f732236e9d4faa1e2c6c
Yacine Laghdiri <yacine.laghdiri@gmail.com>
2024-10-15 20:21:36 +0200

    feat: use config file

--- square.py
+++ square.py
@@ -2,14 +2,7 @@ from typing import List
 import os
 from PIL import Image
 import background
-
-ORG_IMG = "./org"
-DEST_IMG = "./dest"
-
-BORDER_RATIO = 0.05
-ASPECT_RATIO = 1
-REDUCE_FACTOR = 2
-BACKGROUND_TYPE = background.BACKGROUND_TYPE.AVERAGE_DROPSHADOW
+import config
 
 def load_images(path: str):
     images: List[Image.Image] = []
@@ -27,17 +20,17 @@ def save_images(images: List[Image.Image], path: str):
 def squarify(img: Image.Image):
     old_size = img.size
 
-    aspect_multiplicator = ASPECT_RATIO if old_size[0] < old_size[1] else 1/ASPECT_RATIO
+    aspect_multiplicator = config.ASPECT_RATIO if old_size[0] < old_size[1] else 1/config.ASPECT_RATIO
 
-    new_size = (round(max(old_size)*(1 + BORDER_RATIO)*min(aspect_multiplicator, 1)),round(max(old_size)*(1 + BORDER_RATIO)*max(aspect_multiplicator, 1)))
-    new_img = background.get_background(new_size, img, BACKGROUND_TYPE)
+    new_size = (round(max(old_size)*(1 + config.BORDER_RATIO)*min(aspect_multiplicator, 1)),round(max(old_size)*(1 + config.BORDER_RATIO)*max(aspect_multiplicator, 1)))
+    new_img = background.get_background(new_size, img, config.BACKGROUND_TYPE)
     box = tuple((n - o) // 2 for n, o in zip(new_size, old_size))
     new_img.paste(img, box)
 
-    return new_img.reduce(REDUCE_FACTOR)
+    return new_img.reduce(config.REDUCE_FACTOR)
 
 if __name__=="__main__":
-    images = load_images(ORG_IMG)
+    images = load_images(config.ORG_IMG)
     images = [squarify(img) for img in images]
-    save_images(images, DEST_IMG)
+    save_images(images, config.DEST_IMG)
     
